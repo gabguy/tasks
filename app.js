9027d04538c6d1a493a44e6f36fd12f31ed1167b
gabguy <gabguy@gmail.com>
2015-05-10 11:33:40 +0300

    change mongo url

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var express = require('express'),
     mongoose = require('mongoose'),
     bodyParser = require('body-parser');
 
-var database = mongoose.connect('mongodb://localhost/NodeBookAPI');
+var database = mongoose.connect('mongodb://$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/');
 
 var Book = require('./models/bookModel');
 
